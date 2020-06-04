e45a2caa2249c7f8769094c8c705c0d038d41500
vtdung <vtdung@starboardasia.com>
2020-06-04 18:16:23 +0700

    update

---
+++ app/TechQuestion.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TechQuestion extends Model
+{
+    // change default Eloquent Model config
+    protected $table = "t_tech_questions";
+    public $timestamps = false;
+    public $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'content',
+        'type',
+        'del_flg',
+        'date_created',
+        'date_update',
+        'in_del_flg',
+        'in_datecreate',
+        'in_update',
+    ];
+     
+
+}
