0270fec6aee089b412b5f69b3dc1c22709913b21
vtdung <vtdung@starboardasia.com>
2020-06-05 10:19:39 +0700

    add config/constants.php

---
+++ config/constants.php
@@ -0,0 +1,20 @@
+<?php
+    return [
+        'CV_CHANNEL' => [
+            1 => 'IT Viec Apply',
+            2 => '24h Search',
+            3 => 'VietNamWork Apply',
+            4 => 'Introduced',
+            5 => 'Others'
+        ],
+        'STATUS' => [
+            1 => 'Phone&mail contacted',
+            2 => 'Interviewed',
+            3 => 'Cancelled interview',
+            4 => 'Not Pass contacted'
+        ],
+        'LANGUAGE' => [
+            1 => 'PHP',
+            2 => 'C#/ASP.NET'
+        ]
+    ];
