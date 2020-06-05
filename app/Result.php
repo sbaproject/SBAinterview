d1f1084ab4b46d7dd57ee10bf357a25ade3c5c20
vtdung <vtdung@starboardasia.com>
2020-06-05 14:17:55 +0700

    add model

---
+++ app/Result.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Result extends Model
+{
+    // change default Eloquent Model config
+    protected $table = "t_result";
+    public $timestamps = false;
+    public $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'candidate_firstname',
+        'candidate_lastname',
+        'candidate_tel',
+        'candidate_address',
+        'candidate_mail',
+        'candidate_language',
+        'candidate_dob',
+        'starttime',
+        'endtime',
+        'totaltime',
+        'iq_score',
+        'tech_score',
+        'date_created',
+        'date_update'
+    ];
+     
+
+}
