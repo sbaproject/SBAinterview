ef085700d0f1705a744e8308766d8bc04073ee6f
dtnthach <dtnthach@starboardasia.com>
2020-06-28 19:56:58 +0700

    up

--- resources/views/user/includes/header.blade.php
+++ resources/views/user/includes/header.blade.php
@@ -1,4 +1,15 @@
 <header class="header">
+     <div class="user_info">
+            <div class="wrap_box">
+                <div class="text-center text-md-right">
+                    @if (Session::get('user'))
+                        {{ Session::get('user')->u_name }}
+                    @endif
+                     <img src="{{asset('images/user.svg')}}"  class=" " style="width: 50px;margin:0  20px;">
+                    <a id="user-logout1" class="user-logout1" href="{{ asset('/logout')}}">Logout</a>
+                </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -10,4 +21,5 @@
             </div>
         </div>
     </div>
+    
 </header>
