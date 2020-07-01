63a3b976344f85a8b4200d145e80f9703056bf5e
vtdung <vtdung@starboardasia.com>
2020-07-01 16:18:19 +0700

    update admin

--- app/Http/Controllers/InterviewManagementController.php
+++ app/Http/Controllers/InterviewManagementController.php
@@ -212,7 +212,7 @@ class InterviewManagementController extends Controller
             'in_salary' => 'nullable|numeric|digits_between:0,9',
             'in_mail' => 'nullable|email',
 //            'in_tel' => 'nullable|regex:/(0)[0-9]{9}/',
-            'in_tel' => 'nullable|regex:/(^[0-9])[0-9]{9,14}$/',
+            'in_tel' => 'nullable|regex:/(^[\+\x200-9])[\x200-9]{9,14}$/',
             'in_dob' => 'nullable|date_format:Y/m/d'
            // 'in_cvno' => 'required|unique:t_interviewmanagement,in_cvno'
 
@@ -337,7 +337,7 @@ class InterviewManagementController extends Controller
             'in_salary' => 'nullable|numeric|digits_between:0,9',
             'in_mail' => 'nullable|email',
 //            'in_tel' => 'nullable|regex:/(0)[0-9]{9}/',
-            'in_tel' => 'nullable|regex:/(^[0-9])[0-9]{9,14}$/',
+            'in_tel' => 'nullable|regex:/(^[\+\x200-9])[\x200-9]{9,14}$/',
             'in_dob' => 'nullable|date_format:Y/m/d'
 //            'in_cvno'=>[
 //                'required',
